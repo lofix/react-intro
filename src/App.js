2ff33afc7f0943446ada892beb1fc211ebdd1794
lofix <maciekk.milewski@gmail.com>
2018-05-31 09:27:16 +0200

    add simple state handlers and css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 28},
+        { name: 'Manu', age: 29},
+        { name: 'Stephanie', age: 27}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value, age: 29},
+        { name: 'Stephanie', age: 26}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi</h1>
-        <Person name="Maciek" age="27"/>
+        <button onClick ={this.switchNameHandler.bind(this, "MAXXX")} >Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi"));
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import './Person.css';
 class person extends Component {
+
   render(){
-    return <p>I'm a {this.props.name} and I'm {this.props.age} years old!</p>;
+    return (
+      <div>
+        <p className="Person" onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old!</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    );
   }
 }
 
